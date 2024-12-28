bf306b444a448254a9339a7f15fe245a5842404d
Rajshreesingh <36188098+Rajshreesingh@users.noreply.github.com>
2024-12-28 17:56:58 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accoundId = 12345
+let accountEmail ="rajshree@gmail.com"
+var accountPassword ="raj@123"
+ accountCity = "Noida"
+
+ //accoundId = 345657 not allowed
+ accountEmail ="shri@gmail.com"
+ accountPassword = "789@raj"
+ accountCity = "Delhi"
+
+ console.table([accoundId, accountEmail, accountPassword, accountCity])
+ 
+
